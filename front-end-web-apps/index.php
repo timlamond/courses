dc77e69160ae9a6cb79a0753b59ed3c66db75aca
timlamond <tlamond@Tims-iMac.local>
2018-12-11 00:26:54 -0500

    Initial commit of workshop.lamond.ca/courses/

---
+++ front-end-web-apps/index.php
@@ -0,0 +1,11 @@
+<?php
+include $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php";
+?>
+
+<main id="courses" class="centred--wide">
+  <h3 class="projects__heading secondary-heading">Build Front-End Web Applications from Scratch</h3>
+</main>
+
+<?php
+include $_SERVER["DOCUMENT_ROOT"] . "/includes/footer.php";
+?>
