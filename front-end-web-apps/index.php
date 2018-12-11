4df8280c9228cea1935818425b25a6f7da280778
timlamond <tlamond@Tims-iMac.local>
2018-12-11 00:31:54 -0500

    Added Section Heading for Unit 1.

--- front-end-web-apps/index.php
+++ front-end-web-apps/index.php
@@ -4,6 +4,10 @@ include $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php";
 
 <main id="courses" class="centred--wide">
   <h3 class="projects__heading secondary-heading">Build Front-End Web Applications from Scratch</h3>
+
+  <article class="unit">
+    <h4>Unit 1: Introduction to Javascript</h4>
+  </article>
 </main>
 
 <?php
